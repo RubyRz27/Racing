27fa7559050d86f27b093428c8415ccff4940403
RubyRz27 <97379424+RubyRz27@users.noreply.github.com>
2024-06-10 11:14:21 +0700

    save prog

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Method to load the racing scene (nah it's not really a racing scene, it's just there to trick the player
+    public void StartRace()
+    {
+        SceneManager.LoadScene("RacingScene"); // Replace later
+    }
+
+    // Method to load the settings scene
+    public void OpenSettings()
+    {
+        // Load the settings scene or open settings panel
+        SceneManager.LoadScene("SettingsScene"); // Replace "SettingsScene" with the name of your settings scene if you have one
+    }
+
+    // Method to quit the game
+    public void QuitGame()
+    {
+        Application.Quit();
+        // Note: This will not work in the editor. It only works in the built application.
+        Debug.Log("Quit Game"); // This is for testing in the editor
+    }
+}
