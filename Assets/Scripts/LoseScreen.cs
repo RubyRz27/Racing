3c2d6f90cddf5d9133e845701e2f66b17c2f470f
RubyRz27 <dtiendung16@gmail.com>
2024-06-15 16:09:03 +0700

    win/lose screen polished, will build in class in case I need David

---
+++ Assets/Scripts/LoseScreen.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseScreen : MonoBehaviour
+{
+    // Method to load the main menu
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu"); // Replace "MainMenu" with the name of your main menu scene
+    }
+}
